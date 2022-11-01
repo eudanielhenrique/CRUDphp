f5c546a7a815a333b5e7af68a81aa456535f7b8c
Daniel Henrique <ddanielhpf@gmail.com>
2022-11-01 18:32:33 -0300

    Update conn.php

--- config/conn.php
+++ config/conn.php
@@ -1,7 +1,8 @@
 <?php
-    define('HOST', 'localhost');
-    define('USER', 'root');
-    define('PASS', '');
-    define('BASE', 'crudphp');
+    define('HOST', 'db.haksfvisqqvzhboeliqi.supabase.co');
+    define('PORT', '5432 ');
+    define('USER', 'postgres');
+    define('PASS', 'N@mcfq74@MURwBT');
+    define('BASE', 'postgres');
 
-    $conn = new mysqli(HOST, USER, PASS, BASE);
+    $conn = new mysqli(USER, PASS, HOST, PORT,  BASE);
